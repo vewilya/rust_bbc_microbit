5b7459e4b6d535bfd31ae13585fae3e96a338a2e
Urs Bollhalder <77921468+vewilya@users.noreply.github.com>
2025-09-24 10:43:28 +0200

    chapter 6.2

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,17 @@ use rtt_target::{rprintln, rtt_init_print, rdbg};
 use nrf52833_hal::{pac, timer, gpio};
 use embedded_hal::{delay::DelayNs};
 
+
+fn wait() {
+    for _ in 0..4_000_000 {
+        asm::nop();
+    }
+}
+
 #[entry]
 fn main() -> ! {
 
-    
+
     // let mut board = Board::take().unwrap();
     // board.display_pins.col2.set_low().unwrap();
     // board.display_pins.row2.set_high().unwrap();
@@ -24,12 +31,19 @@ fn main() -> ! {
     // Access Peripherals Access Crate (PAC)
     let peripherals = pac::Peripherals::take().unwrap();
     let p0 = gpio::p0::Parts::new(peripherals.P0);
+
     let _row1 = p0.p0_21.into_push_pull_output(gpio::Level::High);
+    let mut row2 = p0.p0_22.into_push_pull_output(gpio::Level::Low);
     let _col1 = p0.p0_28.into_push_pull_output(gpio::Level::Low);
 
     #[allow(clippy::empty_loop)]
     loop {
         asm::nop();
 
+        // For this to work, use cargo run --release
+        row2.set_high().unwrap();
+        wait();
+        row2.set_low().unwrap();
+        wait();
     }
 }
